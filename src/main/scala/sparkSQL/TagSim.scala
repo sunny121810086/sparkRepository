03d9db33c9225bec0ea479d473c50a5e69549566
sunny121810086 <1306743995@qq.com>
2020-09-05 13:23:53 +0800

    更新时间20200905

---
+++ src/main/scala/sparkSQL/TagSim.scala
@@ -0,0 +1,16 @@
+package sparkSQL
+
+import org.apache.spark.sql.SparkSession
+
+object TagSim {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName(this.getClass.getName)
+      .master("local[2]")
+      .getOrCreate()
+    val sc = spark.sparkContext
+
+
+  }
+}
