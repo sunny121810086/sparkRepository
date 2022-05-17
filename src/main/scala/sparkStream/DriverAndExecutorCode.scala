ba2c3b10e369b491e542db0019e30275a0798f8d
sunny121810086 <1306743995@qq.com>
2022-05-18 01:18:10 +0800

    更新时间20220516

--- src/main/scala/sparkStream/DriverAndExecutorCode.scala
+++ src/main/scala/sparkStream/DriverAndExecutorCode.scala
@@ -1,15 +1,15 @@
 package sparkStream
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 
 object DriverAndExecutorCode {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder()
       .appName(this.getClass.getName)
-      .master("local[2]")
+      .master("local[*]")
       .getOrCreate()
     val sc = spark.sparkContext
     val ssc = new StreamingContext(sc, Seconds(5))
@@ -20,7 +20,7 @@ object DriverAndExecutorCode {
     inputDstream.transform {
       rdd => {
         //TODO 代码2 --> Driver端执行 (每采集一次数据，执行一次。如：从redis读取黑名单可以在这里处理)
-        rdd.map {//RDD的算子中传入函数在Driver端执行
+        rdd.map {
           case line => {
             //TODO 代码3 --> Executor端执行 (不同的Executor都会执行这段代码)
           }
@@ -34,13 +34,17 @@ object DriverAndExecutorCode {
       }
     }
 
-    //数据库连接位置选择
-    //1.不能在Driver端,因为连接不能序列化,传入到Executor端
-    //2.foreach中创建，造成资源浪费
-    //3.增加foreachPartition,在分区创建
-
+    /**
+      * 数据库连接位置选择：
+      * 1.根据闭包原则，Driver端对象需要序列化才能通过网络传入到Executor端，
+      *   由于连接不能被序列化，所以不能在Driver端创建
+      * 2.在foreach中创建，每一条数据都会创建一个连接，造成资源浪费
+      * 3.使用foreachPartition算子，而RDD算子中的函数是在Executor端执行的，
+      *  所以在分区中创建较好。
+      **/
     inputDstream.foreachRDD {
       rdd => {
+        //TODO Driver端执行，数据库连接不能被序列化，因此不能在这里创建
         rdd.foreachPartition {
           iter => {
             //TODO  这里创建数据库连接，如redis、mysql等
